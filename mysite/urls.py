1bdb3686ac18ad376c7d59f74cffb126828d173a
Pattarin <pattarin.won@ku.th>
2020-10-31 13:24:11 +0700

    modified path to view

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from polls.views import IndexView
+from polls.views import index_view
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('', index_view, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
